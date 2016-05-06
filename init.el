2329cdf8ed935a95a958c02c6ccf7f055a5b1565
pokotyamu <pokotyamu@gmail.com>
2016-05-06 16:57:35 +0900

    処理が重いので必要なさそうなパッケージを消した

--- init.el
+++ init.el
@@ -1,17 +1,7 @@
+(setq gc-cons-threshold (* 128 1024 1024))
 (require 'package)
-
 ;; MELPAを追加
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
-
-;; MELPA-stableを追加
-(add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/") t)
-
-;; Marmaladeを追加
-(add-to-list 'package-archives  '("marmalade" . "http://marmalade-repo.org/packages/") t)
-
-;; Orgを追加
-(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
-
 
 ;; 初期化
 (package-initialize)
